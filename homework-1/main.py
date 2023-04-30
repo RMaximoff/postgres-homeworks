c9a15397ea8956bd0a17145a4ba9591a2416fb59
Great <great.goodwin.92@gmail.com>
2023-04-30 15:13:46 +0300

    добавлен Скрипт для заполнения данными таблиц в БД Postgres.

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,25 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+connect = psycopg2.connect("host=localhost "
+                           "port=5432 "
+                           "dbname=north "
+                           "user=postgres "
+                           "password=2175")
+cursor = connect.cursor()
+
+queries = {'north_data/employees_data.csv': 'INSERT INTO employees VALUES (default, %s, %s, %s, %s, %s)',
+           'north_data/customers_data.csv': 'INSERT INTO customers VALUES (%s, %s, %s)',
+           'north_data/orders_data.csv': 'INSERT INTO orders VALUES (%s, %s, %s, %s, %s)'}
+
+for i in queries:
+    with open(i, 'r') as file:
+        csvreader = csv.reader(file)
+        next(csvreader)
+        rows = [v for v in csvreader]
+        cursor.executemany(queries[i], rows)
+        connect.commit()
+
+connect.close()
